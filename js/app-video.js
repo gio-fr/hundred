064bc141da2dc6d66ffee4fb2f38d7f12fddcf5b
fabulouse <birba.fabien@gmail.com>
2015-11-17 20:02:06 +0100

    JS controle video
    contrôle lecteur
    affichage temps

---
+++ js/app-video.js
@@ -0,0 +1,98 @@
+var video = document.getElementById("video1");
+var video2 = document.getElementById("video2");
+var progress = document.getElementById("progress");
+var progressnum = document.getElementById("progressnum");
+ var muteBtn = document.getElementById("mute");
+
+//controleur video
+function vidplay() {
+ var button = document.getElementById("play");
+ if (video.paused){
+    (video).play();
+    (video2).play();
+    button.textContent = "||";
+    //console.log('pause');
+ } else {
+    (video).pause();
+    (video2).pause();
+    button.textContent = ">";
+    //console.log('play');
+ }
+}
+
+function restart() {
+  video.currentTime = 0;
+  video2.currentTime = 0;
+  //console.log('restart');
+}
+
+function skip(value) {
+  video.currentTime += value;
+  video2.currentTime += value;
+  //console.log('skip');
+}
+
+function mute(){
+  if (video.muted) {
+      video.muted = false;
+      muteBtn.textContent = "SON";
+    } else {
+      video.muted = true;
+      muteBtn.textContent = "MUTE";
+    }
+  }
+
+  //barre de progression
+  video.ontimeupdate = function(){
+    if(video.readyState < video.HAVE_METADATA)
+    video.play();
+    video.onloadmetadata = function(){
+    video.duration = vd;
+    video.readyState = vr;
+    video.videoWidth = vw;
+    video.currentTime[2] = vc;
+    var vLength;
+    var pgFlag = "";
+    };
+    var maxprogress = 66.5938;
+    var actualprogress = 0;
+    var itv = 0;
+    if(actualprogress >= maxprogress)
+    {
+      clearInterval(itv);
+      return;
+    }
+   actualprogress +=  video.currentTime;
+   var vduration = video.duration;
+   var fraction = actualprogress / vduration;
+   var pourcent = Math.ceil(fraction*100);
+   progress.style.width = pourcent + "%";
+   progressnum.innerHTML = pourcent +"heures";
+   if(actualprogress == maxprogress) clearInterval(itv);
+   //console.log(actualprogress);
+  };
+
+//verification lecture de la video
+if (video.canPlayType) {
+  video.addEventListener("canplay", function () {
+    document.getElementById("NavBtn").style.display = "inline-block";
+  }, false);
+
+  video.addEventListener("loadedmetadata", function () {
+  vLength = video.duration.toFixed(1);
+  document.getElementById("vLen").textContent = vLength;
+  }, false);
+}
+
+video.onplay = function checkVideoSync(){
+  var time1 = video.currentTime,
+      time2 = video2.currentTime;
+  //console.log("video",time1, time2);
+  if(Math.abs(time1 - time2) > 1){
+    var abs = Math.abs(time1 - time2);
+    //console.log(abs);
+    //Seek to lowest index
+    video.currentTime = video2.currentTime = Math.min(time1, time2);
+    //console.console.log('good');
+  }
+};
